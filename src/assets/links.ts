45f9376993e14b7fd7385be9aae5487f1c6e2163
Alice Chou <alice1qaz@gmail.com>
2022-06-09 15:15:00 +0800

    style(add huawei): add huawei

--- src/assets/links.ts
+++ src/assets/links.ts
@@ -10,6 +10,8 @@ export const links = {
     'https://play.google.com/store/apps/details?id=net.metaquotes.metatrader4&hl=en&referrer=ref_id%3d5189084203383832573%26server%3dWenChuanGlobalLtd-Demo%252cWenChuanGlobalLtd-Live',
   mt4IOS: 'https://apps.apple.com/us/app/metatrader-4/id496212596',
   mt4Huawei: 'https://appgallery.huawei.com/#/app/C102015319',
+  mt4HuaweiApk:
+    'https://download.mql5.com/cdn/web/metaquotes.software.corp/mt4/metatrader4.apk?utm_source=www.metatrader4.com&utm_campaign=install.metaquotes',
   mt4APP:
     'https://download.mql5.com/cdn/web/17749/mt4/wenchuangloballtd4setup.exe',
   // 真實帳戶
